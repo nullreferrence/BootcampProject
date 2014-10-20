3a523d338f5995fb057af2e215f4946f89b87e04
sujoysuktoemon <sujoysuktoemon@gmail.com>
2014-10-20 11:22:56 +0600

    okk

---
+++ new/BoothCampStudentCourseApp/BoothCampStudentCourseApp/DAL/DAO/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoothCampStudentCourseApp.DAL.DAO
+{
+    class Course
+    {
+        public int CourseID { get; set; }
+        public string CourseTitle { get; set; }
+        public string CourseName { get; set; }
+        public string EnrollmentDate { get; set; }
+        public int StudentID { get; set; }
+    }
+}
---
+++ new/BoothCampStudentCourseApp/BoothCampStudentCourseApp/DAL/DAO/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoothCampStudentCourseApp.DAL.DAO
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string RegNo { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
